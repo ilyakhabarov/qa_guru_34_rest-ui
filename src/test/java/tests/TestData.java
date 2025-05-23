d6b4a1fa060c335b7097af51bf5fe8c50278d6d9
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-05-24 01:03:11 +0300

    some bugfixes

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -3,5 +3,5 @@ package tests;
 public class TestData {
     public static final String USERNAME = System.getProperty("USERNAME");
     public static final String PASSWORD = System.getProperty("PASSWORD");
-    public static final String BOOK_ISBN = System.getProperty("BOOK_ISBN");
+    public static final String BOOK_ISBN = "9781449325862";
 }
